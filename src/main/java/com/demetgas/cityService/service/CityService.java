b1db7990de1b7281cf69126e91a6d3c7f038f138
demetgas <demet_gas@hotmail.com>
2023-07-25 20:34:43 +0200

    Update CityService.java

--- src/main/java/com/demetgas/cityService/service/CityService.java
+++ src/main/java/com/demetgas/cityService/service/CityService.java
@@ -2,14 +2,16 @@ package com.demetgas.cityService.service;
 
 import com.demetgas.cityService.model.City;
 import com.demetgas.cityService.repository.CityRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class CityService {
 
-    private CityRepository cityRepository;
+    private final CityRepository cityRepository;
     public List<City> getCities() {
     }
 }
