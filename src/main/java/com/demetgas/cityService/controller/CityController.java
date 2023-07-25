fb5c57e005d69db0ebe33e798f721946aa87f247
demetgas <demet_gas@hotmail.com>
2023-07-25 18:04:24 +0200

    added @Pathvariable

--- src/main/java/com/demetgas/cityService/controller/CityController.java
+++ src/main/java/com/demetgas/cityService/controller/CityController.java
@@ -4,6 +4,7 @@ import com.demetgas.cityService.model.City;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class CityController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<City> getCity(){
+    public ResponseEntity<City> getCity(@PathVariable String id){
         return null;
     }
 
