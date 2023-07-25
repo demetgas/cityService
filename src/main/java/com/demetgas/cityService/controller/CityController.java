214a5157df0133c227b46783d6f99663e47ac519
demetgas <demet_gas@hotmail.com>
2023-07-25 17:15:56 +0200

    getCities created

--- src/main/java/com/demetgas/cityService/controller/CityController.java
+++ src/main/java/com/demetgas/cityService/controller/CityController.java
@@ -1,17 +1,27 @@
 package com.demetgas.cityService.controller;
 
 import com.demetgas.cityService.model.City;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/city")
 public class CityController {
-    public ResponseEntity<List<City>> getCities(){
 
-        return null;
+    @GetMapping
+    public ResponseEntity<List<City>> getCities() {
+        City c1 = new City("06","Gilan");
+        City c2 = new City("01","Pristine");
+
+        List<City> cities = Arrays.asList(c1,c2);
+        return new ResponseEntity<>(cities, HttpStatus.OK);
     }
+
 }
+
