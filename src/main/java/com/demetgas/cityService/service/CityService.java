00b7829d265427a9af849cd072a288e9237df4ad
demetgas <demet_gas@hotmail.com>
2023-07-25 20:30:34 +0200

    created new interface

--- src/main/java/com/demetgas/cityService/service/CityService.java
+++ src/main/java/com/demetgas/cityService/service/CityService.java
@@ -1,13 +1,15 @@
 package com.demetgas.cityService.service;
 
 import com.demetgas.cityService.model.City;
+import com.demetgas.cityService.repository.CityRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CityService {
-    
+
+    private CityRepository cityRepository;
     public List<City> getCities() {
     }
 }
