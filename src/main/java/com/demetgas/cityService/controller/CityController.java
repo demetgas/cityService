b1b36ec35d08f5db38816908c782708dd7f2e775
demetgas <demet_gas@hotmail.com>
2023-07-25 16:59:21 +0200

    City class created and @Data imported and getCities created

--- src/main/java/com/demetgas/cityService/controller/CityController.java
+++ src/main/java/com/demetgas/cityService/controller/CityController.java
@@ -1,9 +1,17 @@
 package com.demetgas.cityService.controller;
 
+import com.demetgas.cityService.model.City;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/city")
 public class CityController {
+    public ResponseEntity<List<City>> getCities(){
+
+        return null;
+    }
 }
---
+++ src/main/java/com/demetgas/cityService/model/City.java
@@ -0,0 +1,10 @@
+package com.demetgas.cityService.model;
+
+import lombok.Data;
+
+@Data
+public class City {
+    private String id;
+    private String name;
+
+}
