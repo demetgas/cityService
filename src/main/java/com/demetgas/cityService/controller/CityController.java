159ee11a99e17acda4b55ee72e5cd6f29237efb7
demetgas <demet_gas@hotmail.com>
2023-07-25 16:54:35 +0200

    added two annotations

--- src/main/java/com/demetgas/cityService/controller/CityController.java
+++ src/main/java/com/demetgas/cityService/controller/CityController.java
@@ -1,4 +1,9 @@
 package com.demetgas.cityService.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/city")
 public class CityController {
 }
