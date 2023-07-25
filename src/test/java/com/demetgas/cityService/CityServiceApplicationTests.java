406542689c46ac4f5f74914f2b272e094be6c699
demetgas <demet_gas@hotmail.com>
2023-07-25 16:39:58 +0200

    Initial commit

---
+++ src/test/java/com/demetgas/cityService/CityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demetgas.cityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
